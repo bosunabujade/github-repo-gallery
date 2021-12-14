f056ae7c9de0697b9359ade664c9bd06cc17457f
bosunabujade <bosunabujade@gmail.com>
2021-12-13 21:31:25 -0500

    Added the api to fecth the repos datat from my git hub, and added a function to display them

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // This is targeting the div with the class overview
 const profileOverview = document.querySelector(".overview");
+const repoList = document.querySelector(".repo-list")
 const username = "bosunabujade";
 
 const getProfile = async function() {
@@ -24,4 +25,19 @@ const userInformation= function (profileData) {
         <p><strong>Number of public repos:</strong> ${profileData.public_repos}</p>
     </div>`
     profileOverview.append(userInfoBlock)
+    getRepoList()
+}
+const getRepoList = async function() {
+    const list = await fetch(`https://api.github.com/users/${username}/repos`)
+    const listData = await list.json()
+    displayRepoInfo(listData)
+}
+
+const displayRepoInfo = function(repos) {
+    for (let repo of repos) {
+        let li = document.createElement("li");
+        li.classList.add("repo");
+        li.innerHTML = `<h3> ${repo.name} </h3>`
+        repoList.append(li)
+    }
 }
